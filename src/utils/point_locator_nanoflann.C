e037e828c4b55adcc82b1f1f611504140a8f020f
John Peterson <jwpeterson@gmail.com>
2021-02-10 15:06:02 -0600

    Try with 16/512

--- src/utils/point_locator_nanoflann.C
+++ src/utils/point_locator_nanoflann.C
@@ -40,8 +40,8 @@ PointLocatorNanoflann::PointLocatorNanoflann (const MeshBase & mesh,
                                               const PointLocatorBase * master) :
   PointLocatorBase (mesh, master),
   _out_of_mesh_mode(false),
-  _initial_num_results(8),
-  _max_num_results(1000)
+  _initial_num_results(16),
+  _max_num_results(512)
 {
   this->init();
 }
