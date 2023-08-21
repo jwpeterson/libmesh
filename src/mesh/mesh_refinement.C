f84fb3662e5c9daf8a3ed0d0f9be16f0c57b7a26
John Peterson <jwpeterson@gmail.com>
2023-08-21 13:56:34 -0500

    Comment out eliminate_unrefined_patches() call

--- src/mesh/mesh_refinement.C
+++ src/mesh/mesh_refinement.C
@@ -1620,8 +1620,12 @@ void MeshRefinement::_smooth_flags(bool refining, bool coarsening)
             !refining ||
             this->make_refinement_compatible();
 
-          bool smoothing_satisfied =
-            !this->eliminate_unrefined_patches();// &&
+          // bool smoothing_satisfied =
+          //   !this->eliminate_unrefined_patches();// &&
+
+          // Debugging: skip calling eliminate_unrefined_patches() to
+          // see if it fixes an application issue I'm seeing.
+          bool smoothing_satisfied = true;
 
           if (_edge_level_mismatch_limit)
             smoothing_satisfied = smoothing_satisfied &&
