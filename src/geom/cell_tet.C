82129ca305d73110b41ad5e3223f4865112b950e
John Peterson <jwpeterson@gmail.com>
2024-08-09 10:06:32 -0500

    Revert "Fix issue in Tet4::edge_sides_map"
    This reverts commit 9a58a956bfccfb433819e5daaa9471f43a334bc9.

--- src/geom/cell_tet.C
+++ src/geom/cell_tet.C
@@ -52,10 +52,10 @@ const Real Tet::_master_points[14][3] =
 const unsigned int Tet::edge_sides_map[6][2] =
   {
     {0, 1}, // Edge 0
-    {1, 2}, // Edge 1
-    {2, 0}, // Edge 2
-    {0, 3}, // Edge 3
-    {1, 3}, // Edge 4
+    {0, 2}, // Edge 1
+    {0, 3}, // Edge 2
+    {1, 3}, // Edge 3
+    {1, 2}, // Edge 4
     {2, 3}  // Edge 5
   };
 
