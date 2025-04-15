d41984ef28c1cdca35274b63c038647d9ec37035
John Peterson <jwpeterson@gmail.com>
2025-04-15 13:15:29 -0500

    Reword comment

--- src/mesh/unstructured_mesh.C
+++ src/mesh/unstructured_mesh.C
@@ -1839,14 +1839,14 @@ UnstructuredMesh::stitching_helper (const MeshBase * other_mesh,
 
         for (unsigned i=0; i<2; ++i)
           {
-            // Determine an h_min for mesh_array[i]. We previously
-            // used the first non-zero Elem::hmin() value encountered
-            // in the entire Mesh for this, while updating hmin as
-            // necessary while processing faces and edges on the
-            // actual boundary in question. This doesn't work well for
-            // the case where the boundary nodeset is comprised
-            // entirely (or almost entirely) of NodeElems, since
-            // NodeElems don't have sides or edges.
+            // Determine an h_min value. We previously used the first
+            // non-zero Elem::hmin() value encountered in the entire
+            // Mesh for this, while updating hmin as necessary during
+            // the processing of faces and edges on the actual
+            // boundary in question. This doesn't work well for the
+            // case where the boundary nodeset is comprised entirely
+            // (or almost entirely) of NodeElems, since NodeElems
+            // don't have sides or edges.
 
             // First we deal with node boundary IDs.
             // We only enter this loop if we have at least one
